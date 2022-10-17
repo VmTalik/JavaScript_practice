672124cc102f8804ce7275f60a0864c887296a45
VmTalik <vmarlyan@mail.ru>
2022-10-17 12:26:55 +0300

    Add a task to work with arrays

--- simple_JS_scripts/work_with_arrays.js
+++ simple_JS_scripts/work_with_arrays.js
@@ -61,3 +61,23 @@ function standardizeStrings(arr) {
     })
 }
 standardizeStrings(favoriteCities)
+
+/*
+3) Напишите функцию reverse, которая принимает в себя строку и возвращает эту строку в обратном порядке.
+
+Пример:
+const someString = 'This is some strange string';
+reverse(someString) => 'gnirts egnarts emos si sihT'
+
+Функцию можно применить к любой строке. Если в функцию приходит не строка - вернуть сообщение "Ошибка!"*/
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    if (typeof(str) !== 'string') {
+        return "Ошибка!";
+    }
+    return str.split('').reverse().join('');
+}
+
+console.log(reverse(someString))
